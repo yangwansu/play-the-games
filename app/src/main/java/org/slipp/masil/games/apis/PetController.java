09c75f487de9ba6feb8e5ac7ddce1fad72502bf9
yangwansu <ywsaau77@gmail.com>
2021-05-29 22:46:45 +0900

    swagger setup

---
+++ app/src/main/java/org/slipp/masil/games/apis/PetController.java
@@ -0,0 +1,63 @@
+package org.slipp.masil.games.apis;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+public class PetController implements PetApi{
+    @Override
+    public Optional<ObjectMapper> getObjectMapper() {
+        return Optional.empty();
+    }
+
+    @Override
+    public Optional<HttpServletRequest> getRequest() {
+        return Optional.empty();
+    }
+
+    @Override
+    public ResponseEntity<Void> addPet(Pet body) {
+        return null;
+    }
+
+    @Override
+    public ResponseEntity<Void> deletePet(Long petId, String apiKey) {
+        return null;
+    }
+
+    @Override
+    public ResponseEntity<List<Pet>> findPetsByStatus(List<String> status) {
+        return null;
+    }
+
+    @Override
+    public ResponseEntity<List<Pet>> findPetsByTags(List<String> tags) {
+        return null;
+    }
+
+    @Override
+    public ResponseEntity<Pet> getPetById(Long petId) {
+        return null;
+    }
+
+    @Override
+    public ResponseEntity<Void> updatePet(Pet body) {
+        return null;
+    }
+
+    @Override
+    public ResponseEntity<Void> updatePetWithForm(Long petId, String name, String status) {
+        return null;
+    }
+
+    @Override
+    public ResponseEntity<ModelApiResponse> uploadFile(Long petId, String additionalMetadata, MultipartFile file) {
+        return null;
+    }
+}
