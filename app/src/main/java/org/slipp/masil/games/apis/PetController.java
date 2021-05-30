50cdf9cafbf89d2701f005d0b43e35a0f05d31ca
yangwansu <ywsaau77@gmail.com>
2021-05-30 17:21:36 +0900

    migrate to openapi-generator

--- app/src/main/java/org/slipp/masil/games/apis/PetController.java
+++ app/src/main/java/org/slipp/masil/games/apis/PetController.java
@@ -1,63 +1,25 @@
 package org.slipp.masil.games.apis;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.multipart.MultipartFile;
 
-import javax.servlet.http.HttpServletRequest;
 import java.util.List;
-import java.util.Optional;
 
 @RestController
-public class PetController implements PetApi{
-    @Override
-    public Optional<ObjectMapper> getObjectMapper() {
-        return Optional.empty();
-    }
+public class PetController implements PetsApi{
 
     @Override
-    public Optional<HttpServletRequest> getRequest() {
-        return Optional.empty();
-    }
-
-    @Override
-    public ResponseEntity<Void> addPet(Pet body) {
+    public ResponseEntity<Void> createPets() {
         return null;
     }
 
     @Override
-    public ResponseEntity<Void> deletePet(Long petId, String apiKey) {
+    public ResponseEntity<List<Pet>> listPets(Integer limit) {
         return null;
     }
 
     @Override
-    public ResponseEntity<List<Pet>> findPetsByStatus(List<String> status) {
-        return null;
-    }
-
-    @Override
-    public ResponseEntity<List<Pet>> findPetsByTags(List<String> tags) {
-        return null;
-    }
-
-    @Override
-    public ResponseEntity<Pet> getPetById(Long petId) {
-        return null;
-    }
-
-    @Override
-    public ResponseEntity<Void> updatePet(Pet body) {
-        return null;
-    }
-
-    @Override
-    public ResponseEntity<Void> updatePetWithForm(Long petId, String name, String status) {
-        return null;
-    }
-
-    @Override
-    public ResponseEntity<ModelApiResponse> uploadFile(Long petId, String additionalMetadata, MultipartFile file) {
+    public ResponseEntity<List<Pet>> showPetById(String petId) {
         return null;
     }
 }
